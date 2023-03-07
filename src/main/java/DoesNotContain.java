4b7eb4385457fdf688da04f74da433669fc0cd49
schorij23 <j_schori@aol.com>
2023-03-07 21:57:40 +0000

    Commit

--- src/main/java/DoesNotContain.java
+++ src/main/java/DoesNotContain.java
@@ -15,6 +15,10 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target)
         return false;
     }
+    return true;
+}
 }
